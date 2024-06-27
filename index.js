8af6f8918b422ef519989303a22500aee8b23a85
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-06-27 10:21:11 -0400

    made index.html the default page if none were selected

--- index.js
+++ index.js
@@ -6,15 +6,25 @@ http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "./pages" + q.pathname;
 
+  // If no page was specified, go to index.html by default
+  if (q.pathname === "/") {
+    filename = "./pages/index.html"
+    fs.readFile('./pages/index.html', function(errIndex, dataIndex) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(dataIndex);
+      return res.end();
+    });
+  }
+
   fs.readFile(filename, function(err, data) {
       // If there's an error:
       if (err) {
-      fs.readFile('./pages/404.html', function(err404, data404) {
-        res.writeHead(404, {'Content-Type': 'text/html'});
-        res.write(err404 ? "404 Not Found" : data404);
-        return res.end();
-      });
-    } 
+        fs.readFile('./pages/404.html', function(err404, data404) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.write(err404 ? "404 Not Found" : data404);
+          return res.end();
+        });
+      } 
     // If the filename is found:
     else {
       res.writeHead(200, {'Content-Type': 'text/html'});
