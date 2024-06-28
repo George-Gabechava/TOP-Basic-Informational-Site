b0fc2dfcf6f09eec4b6b798485f8d23442d19d79
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-06-28 14:23:35 -0400

    condensed into one readFile request to avoid 2 being sent

--- index.js
+++ index.js
@@ -4,28 +4,26 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
-  var filename = "./pages" + q.pathname;
-
-  // If no page was specified, go to index.html by default
-  if (q.pathname === "/") {
-    filename = "./pages/index.html"
-    fs.readFile('./pages/index.html', function(errIndex, dataIndex) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(dataIndex);
-      return res.end();
-    });
-  }
+  var filename = "./pages" + (q.pathname === "/" ? "/index.html" : q.pathname);
 
   fs.readFile(filename, function(err, data) {
-      // If there's an error:
-      if (err) {
-        fs.readFile('./pages/404.html', function(err404, data404) {
+    if (err) {
+      // If there's an error
+      fs.readFile('./pages/404.html', function(err404, data404) {
+        // If we fail to get the 404 file, display text.
+        if (err404) {
+          res.writeHead(404, {'Content-Type': 'text/plain'});
+          res.write("404 Not Found");
+        } 
+        // If 404 file is found.
+        else {
           res.writeHead(404, {'Content-Type': 'text/html'});
-          res.write(err404 ? "404 Not Found" : data404);
-          return res.end();
-        });
-      } 
-    // If the filename is found:
+          res.write(data404);
+        }
+        return res.end();
+      });
+    } 
+    // If filename is found.
     else {
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.write(data);
