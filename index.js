af5ee7598ff9d0d44089c496376c89f68fa39855
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-07-11 09:58:52 -0400

    refactored code to use express

--- index.js
+++ index.js
@@ -1,33 +1,30 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
 
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "./pages" + (q.pathname === "/" ? "/index.html" : q.pathname);
+const logger = require("morgan");
 
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      // If there's an error
-      fs.readFile('./pages/404.html', function(err404, data404) {
-        // If we fail to get the 404 file, display text.
-        if (err404) {
-          res.writeHead(404, {'Content-Type': 'text/plain'});
-          res.write("404 Not Found");
-        } 
-        // If 404 file is found.
-        else {
-          res.writeHead(404, {'Content-Type': 'text/html'});
-          res.write(data404);
-        }
-        return res.end();
-      });
-    } 
-    // If filename is found.
-    else {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    }
-  });
-}).listen(8080);
+// Allow static file use in /pages directory
+app.use(express.static("pages"));
+app.use(logger("dev"))
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "index.html"))
+});
+
+app.get("about", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "about.html"))
+});
+
+app.get("contact-me", (req, res) => {
+  res.sendFile(path.join(__dirname, "pages", "contact-me.html"))
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, "pages", "404.html"))
+});
+
+app.listen(3000, function () {
+  console.log(`App listening on port 3000!`);
+});
